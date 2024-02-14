379e1539e6da91f1927107feff9db6d64ce951c2
Jacob A. Morris <jacob.morris@gmail.com>
2024-02-14 13:50:22 -0800

    day 14 code-along done

--- day-14/code-along/src/App.js
+++ day-14/code-along/src/App.js
@@ -1,99 +1,88 @@
 // Import Component
-import React, { Component } from 'react';
-
-// Create a SearchApp Component
-class SearchApp extends Component {
-    // In the constructor, set the initial state of search to empty string
-    // Also *bind `this`* to the handleChange function
-    constructor(props) {
-        super(props);
-
-    }
-
-    // In this event, get the target value, and reset the state of `search`
-    handleChange(event) {
-        // Get event value
-
-        // Set the state to trigger a re-rendering
-
-    }
-
-    // Function to render data
-    render() {
-        // Filter the table data
-        let employees = this.props.data,
-            searchString = this.state == null ? '' : this.state.search.trim().toLowerCase();
-
-        // If the search string isn't an empty string, filter the results using `.match`
-        if (searchString.length > 0) {
-            // We are searching. Filter the results.
-            employees = employees.filter((e) => e.name.toLowerCase().match(searchString));
-        }
-
-        // Return a `div` containing a  `UserInput` component and a `Table` component
-        return (
-            <div>
-            </div>
-        )
-    }
-}
+import React, { useState } from 'react';
 
 // UserInput component
-class UserInput extends Component {
-    constructor(props) {
-        super(props)
-    }
-
+const UserInput = (props) => {
     // Return an input element that, on change, does the event passed to update via props
-    render() {
-        return (
-            <div>
-            </div>)
-    }
+    return (
+        <div>
+          <input 
+            className="form-control mb-2" 
+            placeholder='Search employees...' 
+            onChange={(e) => props.update(e)}
+          />
+        </div>)
+
 }
 
 // Simple TableRow component for showing a <tr>
-class TableRow extends Component {
-    render() {
+const TableRow = (props) => {
         return (
             <tr>
               <td>
-                { this.props.name }
+                { props.name }
               </td>
               <td>
-                { this.props.title }
+                { props.title }
               </td>
-              <td>
-                { this.props.salary }
+              <td>+
+                { props.salary }
               </td>
             </tr>)
-    }
 }
-;
 
-// Class for a table
-class Table extends Component {
-    constructor(props) {
-        super(props);
-    }
-    // Should return a TableRow component for each element in this.props.data
-    render() {
-        return (
-            <div>
-              <table className="table">
-                <tbody>
-                  <tr>
-                    <th>Name</th>
-                    <th>Title</th>
-                    <th>Salary</th>
-                  </tr>
-                  {// write your code here!
-                  }
-                </tbody>
-              </table>
-            </div>
-        )
-    }
+// Function for a table
+const Table = (props) => {
+  // Should return a TableRow component for each element in this.props.data
+  return (
+      <div>
+        <table className="table">
+          <tbody>
+            <tr>
+              <th>Name</th>
+              <th>Title</th>
+              <th>Salary</th>
+            </tr>
+            {// write your code here!
+              props.data.map((d,i) => {
+                return <TableRow key={'person-' + i} name={d.name} salary={d.salary} title={d.title} />
+              })
+            }
+          </tbody>
+        </table>
+      </div>
+  )
+}
+
+// Create a SearchApp Component
+const SearchApp = (props) => {
+  // In the constructor, set the initial state of search to empty string
+  const [search, setSearch] = useState('');
+  
+
+  // In this event, get the target value, and reset the state of `search`
+  const handleChange = (event) => {
+      // Get event value and set state of Search to trigger a re-rendering
+      setSearch(event.target.value);
+  }
+
+  // Filter the table data
+  let employees = props.data,
+  searchString = search.trim().toLowerCase();
+
+  // If the search string isn't an empty string, filter the results using `.match`
+  if (searchString.length > 0) {
+    // We are searching. Filter the results.
+    employees = employees.filter((e) => e.name.toLowerCase().includes(searchString));
+  }
+
+    // Return a `div` containing a  `UserInput` component and a `Table` component
+    return (
+        <div>
+          <UserInput update={handleChange} />
+          <Table data={employees} />
+        </div>
+    )
 }
 
 // Export the SearchApp
