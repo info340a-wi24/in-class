f565c9e8973b0fe6e972d4330a518b92040cb5bd
Jacob - Instruction <jacob@UW-MacBook-Pro.local>
2024-02-26 16:47:35 -0800

    day16 update:

--- day-16/src/Homepage.js
+++ day-16/src/Homepage.js
@@ -1,10 +1,7 @@
 import React from 'react';
-import { Jumbotron } from 'reactstrap';
 
 export default function Homepage() {
-    return (
-        <Jumbotron>
+    return (     
             <h1>Jacob A. Morris</h1>
-        </Jumbotron>
     );
 }
